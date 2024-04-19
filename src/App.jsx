744d529969ea659cf4d97bd6be3eb12df128acca
jasnmar <jasnmar@gmail.com>
2024-04-19 12:49:46 -0400

    First Use State

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-//import { useState } from 'react'
-/**
- * Challenge: 
- * - Create a Meme component.
- * - Inside the Meme component, render a styled form
- *   with our 2 inputs and the button.
- * - Don't worry about adding any functionality yet
- */
+import { useState } from 'react'
+    /**
+     * Challenge: Replace our hard-coded "Yes" on the page with 
+     * some state initiated with React.useState()
+     */
 
 import './App.css'
 import Header from './components/Header/Header'
 import Meme from './components/Meme/Meme'
 
 function App() {
-  //const [count, setCount] = useState(0)
-
+  const result = useState("YNoes")
+  console.log('result: ', result)
   return (
     <>
       <Header />
       <Meme />
+      <h1>{result}</h1>
     </>
   )
 }
