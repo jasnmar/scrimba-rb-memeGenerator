62d07cb8949f248ba6353f7a48672d24a1843b4e
jasnmar <jasnmar@gmail.com>
2024-04-19 11:17:25 -0400

    Get an image

--- src/components/Meme/Meme.jsx
+++ src/components/Meme/Meme.jsx
@@ -1,39 +1,57 @@
 import "./Meme.css"
-
+import memesData from "../../memesData.js"
+    /**
+     * Challenge: Get a random image from the `memesData` array
+     * when the "new meme image" button is clicked.
+     * 
+     * Log the URL of the image to the console. (Don't worry
+     * about displaying the image yet)
+     */
 function Meme() {
+
+    function getImage() {
+        const memeList = memesData.data.memes
+        const randomMeme = Math.floor(Math.random() * memeList.length);
+        const selectedMemeURL = memeList[randomMeme].url
+        console.log('selectedMemeURL: ', selectedMemeURL)
+
+    }
     return (
         <main>
-            <form>
-                <div className="meme--text-inputs">
-                    <div className="meme--input-group">
-                        <label 
-                            className="meme--label" 
-                            htmlFor="meme--top-text">
-                                Top text
-                        </label>
-                        <input 
-                            className="meme--text-input" 
-                            id="meme--top-text" 
-                            type="text" 
-                            name="top" 
-                            placeholder="Top Text"></input>
+            <div className="meme--text-inputs">
+                <div className="meme--input-group">
+                    <label 
+                        className="meme--label" 
+                        htmlFor="meme--top-text">
+                            Top text
+                    </label>
+                    <input 
+                        className="meme--text-input" 
+                        id="meme--top-text" 
+                        type="text" 
+                        name="top" 
+                        placeholder="Top Text"></input>
+                </div>
+                <div className="meme--input-group">
+                    <label 
+                        className="meme--label" 
+                        htmlFor="meme--bottom-text">
+                            Top text
+                    </label>
+                    <input 
+                        className="meme--text-input" 
+                        id="meme--bottom-text" 
+                        type="text" 
+                        name="bottom" 
+                        placeholder="Bottom Text"></input>
                     </div>
-                    <div className="meme--input-group">
-                        <label 
-                            className="meme--label" 
-                            htmlFor="meme--top-text">
-                                Top text
-                        </label>
-                        <input 
-                            className="meme--text-input" 
-                            id="meme--bottom-text" 
-                            type="text" 
-                            name="bottom" 
-                            placeholder="Bottom Text"></input>
-                        </div>
-                    </div>
-                <button className="meme-submit-btn" type="submit">Get a new meme image  🖼</button>
-            </form>
+                </div>
+            <button 
+                onClick={getImage} className="meme-submit-btn" 
+                type="submit">
+                    Get a new meme image  🖼
+            </button>
+
         </main>
     )
 }
